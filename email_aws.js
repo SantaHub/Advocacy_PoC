f7a488a49502ce23b19c7a50f17f7bdee901970b
Austin <ap84321N@pace.edu>
2021-12-15 20:34:47 -0500

    Email AWS poc up and running

---
+++ email_aws.js
@@ -0,0 +1,51 @@
+// Load the AWS SDK for Node.js
+var AWS = require('aws-sdk');
+// Set the region 
+AWS.config.update({region: 'us-east-2'});
+
+// Create sendEmail params 
+var params = {
+  Destination: { /* required */
+    CcAddresses: [
+      'scldeveloper1@gmail.com',
+      /* more items */
+    ],
+    ToAddresses: [
+    //   '',
+      /* more items */
+    ]
+  },
+  Message: { /* required */
+    Body: { /* required */
+      Html: {
+       Charset: "UTF-8",
+       Data: "HTML_FORMAT_BODY"
+      },
+      Text: {
+       Charset: "UTF-8",
+       Data: "TEXT_FORMAT_BODY"
+      }
+     },
+     Subject: {
+      Charset: 'UTF-8',
+      Data: 'Test email'
+     }
+    },
+  Source: 'austin.puthen@pace.edu', /* required */
+  ReplyToAddresses: [
+     'austin.puthen@pace.edu',
+    /* more items */
+  ],
+};
+
+// Create the promise and SES service object
+var sendPromise = new AWS.SES({apiVersion: '2010-12-01'}).sendEmail(params).promise();
+
+// Handle promise's fulfilled/rejected states
+sendPromise.then(
+  function(data) {
+    console.log(data.MessageId);
+  }).catch(
+    function(err) {
+    console.error(err, err.stack);
+  });
